5ed695e721f371ce0ffe7fea93cda1142ae36feb
mabreukla <102083799+mabreukla@users.noreply.github.com>
2022-04-19 16:33:07 -0400

    Implemented the GetAll, GetById, Update methods for the HeroesController.

--- HelloWorldAngularNet6/Controllers/HeroesController.cs
+++ HelloWorldAngularNet6/Controllers/HeroesController.cs
@@ -1,4 +1,5 @@
-﻿using HelloWorldAngularNet6.Models;
+﻿using HelloWorldAngularNet6.Classes;
+using HelloWorldAngularNet6.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,59 @@ namespace HelloWorldAngularNet6.Controllers
     [ApiController]
     public class HeroesController : ControllerBase
     {
-        public HeroesController()
+        HelloWorldContext _db;
+
+        public HeroesController(HelloWorldContext helloWorldContext)
         {
+            _db = helloWorldContext;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<Hero[]> GetAll()
+        {
+            List<Hero> returnValue = new List<Hero>();
+
+            // Manual
+            //returnValue = _db.Set<Hero>().ToList<Hero>();
+
+            // Auto
+            returnValue = _db.Heroes.ToList<Hero>();
+
+            return Ok(returnValue);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Hero> GetById(int id)
+        {
+            Hero returnValue = new Hero();
+
+            Hero foundHero = _db.Heroes.Where(x => x.Id == id).FirstOrDefault();
+            if (foundHero != null)
+            {
+                returnValue = foundHero;
+            }
+
+            return Ok(returnValue);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult<Hero> Update(Hero hero)
+        {
+            _db.Heroes.Update(hero);
+            _db.SaveChanges();
+
+            Hero returnValue = new Hero();
+            Hero addedHero = _db.Heroes.Where<Hero>(x => x.Id == hero.Id).FirstOrDefault();
+            if (addedHero != null)
+            {
+                returnValue = addedHero;
+            }
+
+            return Ok(returnValue);
         }
     }
 }
